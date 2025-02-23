74c80de0ff527f987af9f349ef5fcb250c2105f6
Sam Wolfand <sam.wolfand@gmail.com>
2025-02-22 16:04:06 -0800

    up to date

--- app/src/main/java/com/swolf/greatchase/feature/stats/StatsScreen.kt
+++ app/src/main/java/com/swolf/greatchase/feature/stats/StatsScreen.kt
@@ -1,16 +1,34 @@
 package com.swolf.greatchase.feature.stats
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.swolf.greatchase.ui.theme.BackgroundBlack
 
 @Composable
-fun StatsScreen() {
-  Column(modifier = Modifier.fillMaxSize()) {  }
-
-
+fun StatsScreen(modifier: Modifier = Modifier) {
+  Column(modifier = Modifier.fillMaxSize().then(modifier).background(color = BackgroundBlack)) {}
 }
 
+@Composable
+fun StatsBlock(modifier: Modifier = Modifier) {
+  Box(modifier = Modifier.clip(RoundedCornerShape(8.dp)).then(modifier)) {
 
+    Text()
+
+  }
+}
+
+@Preview
+@Composable
+private fun PreviewStatsScreen() {
+  StatsScreen()
+}
